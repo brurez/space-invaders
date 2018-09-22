924695bf47e5ba3c0ac3e9fe4e8456d0c1b862e7
brurez <brunoderezende@hotmail.com>
2018-09-22 16:35:00 -0300

    Pause and restart

--- main.cpp
+++ main.cpp
@@ -4,24 +4,37 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(800, 640), "Space Invaders", sf::Style::Close);
     window.setVerticalSyncEnabled(true);
 
+    sf::Clock clock;
+    bool isPlaying = false;
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
+            // Window closed or escape key pressed: exit
             if ((event.type == sf::Event::Closed) ||
                 ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))) {
                 window.close();
                 break;
             }
+
+            // Space key pressed: play
+            if( (event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Space)) {
+                if( !isPlaying ) {
+                    // restart game
+                    isPlaying = true;
+                    clock.restart();
+                }
+            }
         }
 
         window.clear();
 
-        /*if (isPlaying) {
+        if (isPlaying) {
+            // draw world
 
-
-        }else{
-
-        }*/
+        } else {
+            //is paused
+        }
 
 
         window.display();
