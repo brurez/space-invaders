7d8012a933871663f4be440135147206b8da5804
brurez <brunoderezende@hotmail.com>
2018-09-22 18:01:30 -0300

    Adds Entity class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,45 @@
 #include <SFML/Graphics.hpp>
+#include <string>
+
+class Position {
+public:
+    double x;
+    double y;
+};
+
+class Entity {
+public:
+    void init(Position position);
+
+    void move(Position position);
+
+    Position getPosition(void);
+
+    Entity(sf::RenderWindow *win, std::string imgFile);
+
+    void draw(void);
+
+private:
+    sf::RenderWindow *window;
+    sf::Sprite sprite;
+    sf::Texture texture;
+};
+
+Entity::Entity(sf::RenderWindow *win, std::string imgFile) {
+    window = win;
+    texture.loadFromFile(imgFile);
+    sprite.setTexture(texture);
+}
+
+void Entity::draw() {
+    window->draw(sprite);
+}
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 640), "Space Invaders", sf::Style::Close);
     window.setVerticalSyncEnabled(true);
+    std::string shipImage = "assets/ship.png";
+    Entity spaceShip(&window, shipImage);
 
     sf::Clock clock;
     bool isPlaying = false;
@@ -18,8 +55,8 @@ int main() {
             }
 
             // Space key pressed: play
-            if( (event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Space)) {
-                if( !isPlaying ) {
+            if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Space)) {
+                if (!isPlaying) {
                     // restart game
                     isPlaying = true;
                     clock.restart();
@@ -27,13 +64,28 @@ int main() {
             }
         }
 
+        if (isPlaying) {
+            float deltaTime = clock.restart().asSeconds();
+
+            // Move ship
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+
+            }
+
+            // Check collision
+
+        }
+
         window.clear();
 
         if (isPlaying) {
-            // draw world
+            spaceShip.draw();
 
         } else {
-            //is paused
         }
 
 
